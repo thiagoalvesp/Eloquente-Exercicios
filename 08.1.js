ae1493aed8cb1e7bf1c8eb221a51d616b63dccd2
thiagoalvesp <thiago-alvesp@live.com>
2017-01-25 12:13:49 -0200

    Exercicio 8.1

---
+++ 08.1.js
@@ -0,0 +1,14 @@
+function MultiplicatorUnitFailure() {}
+
+function primitiveMultiply(a, b) {
+    if (Math.random() < 0.5)
+        return a * b;
+    else
+        throw new MultiplicatorUnitFailure();
+}
+
+function reliableMultiply(a, b) {
+  // Coloque seu código aqui.
+}
+
+console.log(reliableMultiply(8, 8)); // 64
